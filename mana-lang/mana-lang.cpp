a80d0bb877de08e9fa6df93b9c95134c21a4e7fd
Nyxa <arnyxxa@gmail.com>
2021-11-07 02:14:51 +0100

    Add project files.

---
+++ mana-lang/mana-lang.cpp
@@ -0,0 +1,13 @@
+﻿// mana-lang.cpp : Defines the entry point for the application.
+//
+
+#include "mana-lang.h"
+#include "test_module.h"
+
+using namespace std;
+
+int main()
+{
+	cout << hi() << endl;
+	return 0;
+}
---
+++ mana-lang/test_module.cpp
@@ -0,0 +1,6 @@
+#include "test_module.h"
+
+const std::string hi()
+{
+	return "hey\n";
+}
