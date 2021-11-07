d208a89dee231fae60ea4992bb281d0ee53afc68
Nyxa <arnyxxa@gmail.com>
2021-11-07 14:37:32 +0100

    double blep

--- src/mana-lang.cpp
+++ src/mana-lang.cpp
@@ -1,14 +1,8 @@
-﻿// mana-lang.cpp : Defines the entry point for the application.
-//
-
-#include "mana-lang.h"
+﻿#include "mana-lang.h"
 #include "test_module.h"
-
-using namespace std;
 
 int main()
 {
-	// commit test
-	cout << hi() << endl;
+	std::cout << hi() << std::endl;
 	return 0;
 }
